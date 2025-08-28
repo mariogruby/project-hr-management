5f5ea13d0e54563151e205689099cab74523fa1a
mario ruby <mariogabrielrn@gmail.com>
2025-08-28 18:09:13 +0200

    borrado npm y solo se usa yarn, e implementado morgan para la consola

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
+import morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
