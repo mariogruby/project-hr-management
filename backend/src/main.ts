c8df207704edf553cdcfc624e2aa4b6284e0c8e8
mario ruby <mariogabrielrn@gmail.com>
2025-08-29 15:33:56 +0200

    arreglado error de creacion de package lock de npm, solo se usa yarn, y agregado cors config en main.ts, esta harcodeado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  })
+
   app.use(morgan('dev'));
 
   await app.listen(process.env.PORT ?? 3000);
